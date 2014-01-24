e7443454315a6dd23823bcf3a937d207ed05cf94
Weston Odom <weston.odom@gmail.com>
2014-01-24 15:37:17 -0600

    adjusted unit tests to use new sample input name

--- testdeckparser.py
+++ testdeckparser.py
@@ -4,7 +4,7 @@ import unittest
 class TestDeckParser(unittest.TestCase):
 
 	def test_deck(self):
-		deck = DeckParser("sample_input.txt").deck
+		deck = DeckParser("netrunnerdb_sample.txt").deck
 		deck_length = len(deck)
 
 		self.assertEqual(len(deck), deck_length)
@@ -14,7 +14,7 @@ class TestDeckParser(unittest.TestCase):
 			self.assertTrue(deck.count(card) > 0)
 
 	def test_draw(self):
-		deck_parser = DeckParser("sample_input.txt")
+		deck_parser = DeckParser("netrunnerdb_sample.txt")
 		one = deck_parser.draw()
 		ten = deck_parser.draw(num_cards=10)
 
@@ -22,13 +22,13 @@ class TestDeckParser(unittest.TestCase):
 		self.assertEqual(len(ten), 10)
 
 	def test_sample_hand(self):
-		deck_parser = DeckParser("sample_input.txt")
+		deck_parser = DeckParser("netrunnerdb_sample.txt")
 		hand = deck_parser.sample_hand()
 
 		self.assertEqual(len(hand), 5)
 
 	def test_repeated_sample_hands(self):
-		deck_parser = DeckParser("sample_input.txt")
+		deck_parser = DeckParser("netrunnerdb_sample.txt")
 		deck_length = len(deck_parser.deck)
 		for i in range(10):
 			deck_parser.sample_hand()
@@ -36,7 +36,7 @@ class TestDeckParser(unittest.TestCase):
 			self.assertEqual(len(deck_parser.deck), deck_length - 5)
 
 	def test_deck_reset(self):
-		deck_parser = DeckParser("sample_input.txt")
+		deck_parser = DeckParser("netrunnerdb_sample.txt")
 		deck_length = len(deck_parser.full_deck)
 		deck_parser.draw(num_cards=4)
 		self.assertEqual(len(deck_parser.deck), 41)
