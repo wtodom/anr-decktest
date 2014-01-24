31634b46846f4799f14f23f02cdad7d8eccb3bb2
Weston Odom <weston.odom@gmail.com>
2014-01-24 13:25:25 -0600

    handsim now only needs a filename/path as input

--- handsim.py
+++ handsim.py
@@ -1,7 +1,6 @@
 from deckparser import *
 
-with open("sample_input.txt", "r") as f:
-	deck = f.readlines()
-	d = DeckParser(deck)
-	for card in d.full_deck:
-		print(card)
+
+d = DeckParser("sample_input.txt")
+for card in d.full_deck:
+	print(card)
